da74bfe9f95c06d319074b7ec9d36ccf2e4b2245
bharath2023ht <126450752+bharath2023ht@users.noreply.github.com>
2024-05-30 21:02:05 +0530

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/bmicalculator/",
   plugins: [react()],
-  base: "/vite-react-deploy/"
-})
+});
